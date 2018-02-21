e3e81c66f3220a38b9bb944f6630f45ab421d656
guillaume <guillaume.everarts@student.uclouvain.be>
2018-02-21 15:22:56 +0100

    Classe Slice completed

--- src/Pizza.java
+++ src/Pizza.java
@@ -13,7 +13,8 @@ public class Pizza {
     private final boolean MUSH = false;
     private final boolean TOM = true;
 
-    private int maxSize;
+    private int maxSize; //Le nbre d'elem max par tranche
+    private int minElem; //Le nbre d'elem min de chaque sorte
 
     public class Slice {
 
@@ -29,48 +30,110 @@ public class Pizza {
             this.tom = tom;
         }
 
+        public boolean isComplete() {
+            return mush > minElem && tom > minElem;
+        }
+
         public boolean extend(int direction) {
             switch(direction) {
-                case TOP:
-                    if(topleft.y == 0) return false;
-                    for(int i = topleft.x; i <= botright.x; i++)
-                        if(used[i][topleft.y - 1]) return false;
-                    topleft.y--;
-                    for(int i = topleft.x; i <= botright.x; i++) {
-                        used[i][topleft.y] = true;
-                        if(content[i][topleft.y] == TOM) tom++;
-                        else mush++;
-                    }
-                    break;
-                case BOTTOM:
-                    if(botright.y == used[0].length - 1) return false;
-                    for(int i = topleft.x; i <= botright.x; i++)
-                        if(used[i][botright.y + 1]) return false;
-                    topleft.y++;
-                    for(int i = topleft.x; i <= botright.x; i++) {
-                        used[i][botright.y] = true;
-                        if(used[i][botright.y] == TOM) tom++;
-                        else mush++;
-                    }
-                    break;
-                case LEFT:
-                    if(topleft.x == 0) return false;
-                    for(int i = topleft.y; i <= botright.y; i++)
-                        if(used[topleft.x - 1][i]) return false;
-                    topleft.x--;
-                    for(int i = topleft.y; i <= botright.y; i++)
-                        used[topleft.x][i] = true;
-                    break;
-                case RIGHT:
-                    if(botright.x == used.length) return false;
-                    for(int i = topleft.y; i <= botright.y; i++)
-                        if(used[botright.x + 1][i]) return false;
-                    topleft.x++;
-                    for(int i = topleft.y; i <= botright.y; i++)
-                        used[botright.x][i] = true;
-                    break;
+                case TOP: return extTop();
+                case BOTTOM: return extBot();
+                case LEFT: return extLeft();
+                case RIGHT: return extRight();
             }
             return true;
+        }
+
+        private boolean extTop() {
+            if(topleft.y == 0) return false;
+            if(size() + botright.x - topleft.x > maxSize) return false;
+            for(int i = topleft.x; i <= botright.x; i++)
+                if(used[i][topleft.y - 1]) return false;
+            topleft.y--;
+            for(int i = topleft.x; i <= botright.x; i++) {
+                used[i][topleft.y] = true;
+                if(content[i][topleft.y] == TOM) tom++;
+                else mush++;
+            }
+            return true;
+        }
+
+        private boolean extBot() {
+            if(botright.y == used[0].length - 1) return false;
+            if(size() + botright.x - topleft.x > maxSize) return false;
+            for(int i = topleft.x; i <= botright.x; i++)
+                if(used[i][botright.y + 1]) return false;
+            topleft.y++;
+            for(int i = topleft.x; i <= botright.x; i++) {
+                used[i][botright.y] = true;
+                if(content[i][botright.y] == TOM) tom++;
+                else mush++;
+            }
+            return true;
+        }
+
+        private boolean extLeft() {
+            if(topleft.x == 0) return false;
+            if(size() + botright.y - topleft.y > maxSize) return false;
+            for(int i = topleft.y; i <= botright.y; i++)
+                if(used[topleft.x - 1][i]) return false;
+            topleft.x--;
+            for(int i = topleft.y; i <= botright.y; i++) {
+                used[topleft.x][i] = true;
+                if(content[topleft.x][i] == TOM) tom++;
+                else mush++;
+            }
+            return true;
+        }
+
+        private boolean extRight() {
+            if(botright.x == used.length) return false;
+            if(size() + botright.y - topleft.y > maxSize) return false;
+            for(int i = topleft.y; i <= botright.y; i++)
+                if(used[botright.x + 1][i]) return false;
+            topleft.x++;
+            for(int i = topleft.y; i <= botright.y; i++) {
+                used[botright.x][i] = true;
+                if(content[botright.x][i] == TOM) tom++;
+                else mush++;
+            }
+            return true;
+        }
+
+        public int size() {
+            return (botright.x - topleft.x)*(botright.y - topleft.y);
+        }
+
+        public Point getTopleft() {
+            return topleft;
+        }
+
+        public void setTopleft(Point topleft) {
+            this.topleft = topleft;
+        }
+
+        public Point getBotright() {
+            return botright;
+        }
+
+        public void setBotright(Point botright) {
+            this.botright = botright;
+        }
+
+        public int getMush() {
+            return mush;
+        }
+
+        public void setMush(int mush) {
+            this.mush = mush;
+        }
+
+        public int getTom() {
+            return tom;
+        }
+
+        public void setTom(int tom) {
+            this.tom = tom;
         }
     }
 }
