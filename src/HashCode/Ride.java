5723142e937e47e83a4ee74d3eb6ae3ae8a77e46
bnothomb <bastien.nothomb@student.uclouvain.be>
2018-03-01 19:25:26 +0100

    Ride

--- src/HashCode/Ride.java
+++ src/HashCode/Ride.java
@@ -1,4 +1,35 @@
 package HashCode;
 
+import java.awt.*;
+
 public class Ride {
+
+    private int timeStart;
+    private int timeEnd;
+    private Point pointStart;
+    private Point pointEnd;
+
+    public Ride(int timeStart,int timeEnd,Point pointStart,Point pointEnd)
+    {
+        this.timeStart = timeStart;
+        this.timeEnd = timeEnd;
+        this.pointStart = pointStart;
+        this.pointEnd = pointEnd;
+    }
+    public int getTimeStart() {
+        return timeStart;
+    }
+
+    public int getTimeEnd() {
+        return timeEnd;
+    }
+
+    public Point getPointStart() {
+        return pointStart;
+    }
+
+    public Point getPointEnd() {
+        return pointEnd;
+    }
+
 }
