a5ff1ecfb0cbce59a196a75d69fc680e6555111e
gregoire-moreau <gregoire.moreau@student.uclouvain.be>
2018-03-01 19:46:15 +0100

    Main et IO

--- src/HashCode/IO.java
+++ src/HashCode/IO.java
@@ -1,4 +1,61 @@
 package HashCode;
 
+
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class IO {
+
+    public static void read(String in)
+    {
+        try {
+            Scanner scanner = new Scanner(new File(in));
+            Main.numOfRows = scanner.nextInt();
+            Main.numOfCols = scanner.nextInt();
+            Main.numOfTaxis = scanner.nextInt();
+            Main.numOfRides = scanner.nextInt();
+            Main.bonus = scanner.nextInt();
+            Main.numOfSteps = scanner.nextInt();
+            Main.listOfRides = new LinkedList<>();
+            Main.listOfTaxis = new LinkedList<>();
+
+            scanner.nextLine();
+
+            for (int i = 0; i < Main.numOfRides; i++) {
+                Main.listOfRides.add(new Ride(i,
+                        new Point(scanner.nextInt(), scanner.nextInt()),
+                        new Point(scanner.nextInt(), scanner.nextInt()),
+                        scanner.nextInt(),
+                        scanner.nextInt()));
+            }
+
+
+            for(int i = 0; i<Main.numOfTaxis; i++)
+            {
+                Main.listOfTaxis.add(new Taxi());
+            }
+        }
+        catch (FileNotFoundException e){}
+    }
+
+
+    public static void write()
+    {
+        for(Taxi e: Main.listOfTaxis)
+        {
+            if (!e.done.isEmpty())
+            {
+                System.out.print(e.id);
+                for(Ride x : e.done)
+                {
+                    System.out.print(" "+x.getId());
+                }
+                System.out.println();
+            }
+        }
+    }
 }
--- src/HashCode/Main.java
+++ src/HashCode/Main.java
@@ -1,4 +1,24 @@
 package HashCode;
 
+import java.util.LinkedList;
+
 public class Main {
+
+    public static LinkedList<Taxi> listOfTaxis;
+    public static LinkedList<Ride> listOfRides;
+    public static int numOfRows;
+    public static int numOfCols;
+    public static int numOfTaxis;
+    public static int numOfRides;
+    public static int bonus;
+    public static int numOfSteps;
+
+
+
+    public static void main(String[] args)
+    {
+        IO.read(args[0]);
+
+        IO.write();
+    }
 }
