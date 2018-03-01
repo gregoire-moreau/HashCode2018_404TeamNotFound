519f92e81141e0daa06fc851b777f291c7b09eaa
guillaume <guillaume.everarts@student.uclouvain.be>
2018-03-01 21:05:52 +0100

    MainMain

---
+++ src/MainMain.java
@@ -0,0 +1,12 @@
+import HashCode.Main;
+
+public class MainMain {
+
+    public static void main(String[] args) {
+        Main.main(new String[] {"a_example"});
+        Main.main(new String[] {"b_should_be_easy"});
+        Main.main(new String[] {"c_no_hurry"});
+        Main.main(new String[] {"d_metropolis"});
+        Main.main(new String[] {"e_high_bonus"});
+    }
+}
